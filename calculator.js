def0794d1048daefc310747bbf5dc5c1750084ea
sanjay <sanjay.kumar@dianapps.com>
2025-03-30 22:42:26 +0530

    Step 2: Handle any amount of numbers in string

--- calculator.js
+++ calculator.js
@@ -1,5 +1,15 @@
 const add = (str) => {
-    if (!str) return 0;
+  if (!str) return 0;
+
+  // Split the string by comma and map each element to an integer
+  let numbers = str.split(",").map((n) => parseInt(n));
+
+  // Add each number in the array
+  let sum = numbers.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+
+  return sum;
 };
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,15 @@ const add = require('./calculator');
 test("returns 0 for empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("returns number for single number string", () => {
+    expect(add("1")).toBe(1);
+});
+
+test("returns sum of two numbers", () => {
+    expect(add("1,2")).toBe(3);
+});
+
+test("returns sum of multiple numbers", () => {
+    expect(add("1,2,3,4,55")).toBe(65);
+});
